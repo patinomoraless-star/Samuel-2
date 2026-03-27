591c5ca8999b12b9e248ec2d0da0792ca01f264a
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-03-27 16:28:38 -0500

    Agregando script de build

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3', 
+        conn_max_age=600
+    )
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
